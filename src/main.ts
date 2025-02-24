5ac1200a48b22fda1524c68a57f6ac9acaa62c1b
Luca Sartore <lucasar.com8@gmail.com>
2025-02-24 10:57:31 +0100

    Finish first simple server

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,55 @@
-import { spellCheckDocument } from 'cspell-lib';
-import type { ValidationIssue } from 'cspell-lib';
-import type {SpellRequest, PartialLintRequest} from './types/requests.ts';
-import type {SpellResponse, LintResponse} from './types/responses.ts';
-import { convertLintResult } from './util/lintResultConverter.ts';
-import readline from 'node:readline'
+import { spellCheckDocument } from "cspell-lib";
+import type { ValidationIssue } from "cspell-lib";
+import type { SpellRequest, PartialLintRequest } from "./types/requests.ts";
+import type { SpellResponse, LintResponse } from "./types/responses.ts";
+import { convertLintResult } from "./util/lintResultConverter.ts";
+import readline from "node:readline";
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+	input: process.stdin,
+	output: process.stdout,
 });
 
-async function processPartialLintRequest(request: PartialLintRequest ){
-  const result = await spellCheckDocument(
-    { uri: '', text: request.text, languageId: request.languageId, locale: 'en' },
-    { noConfigSearch: true },
-    { }
-  );
-  return convertLintResult(result.issues, request.startLine)
+async function processPartialLintRequest(request: PartialLintRequest) {
+	const result = await spellCheckDocument(
+		{
+			uri: "",
+			text: request.text,
+			languageId: request.languageId,
+			locale: "en",
+		},
+		{ noConfigSearch: true },
+		{},
+	);
+    console.log("test");
+	return convertLintResult(result.issues, request.startLine);
 }
 
 async function checkSpelling(phrase: string): Promise<Array<ValidationIssue>> {
-  const result = await spellCheckDocument(
-    { uri: '', text: phrase, languageId: 'plaintext', locale: 'en,it' },
-    { noConfigSearch: true },
-    { }
-  );
-  return result.issues;
+	const result = await spellCheckDocument(
+		{ uri: "", text: phrase, languageId: "plaintext", locale: "en,it" },
+		{ noConfigSearch: true },
+		{},
+	);
+	return result.issues;
 }
 
-
-async function processRequest(request: SpellRequest): Promise<SpellResponse>{
-    switch (request.Kind){
-        case "partial":
-            return await processPartialLintRequest(request);
-        default:
-            return {
-                kind: "error",
-                message: `Unknown request kind: ${request.Kind}`
-            }
-    }
+async function processRequest(request: SpellRequest): Promise<SpellResponse> {
+	switch (request.Kind) {
+		case "partial":
+			return await processPartialLintRequest(request);
+		default:
+			return {
+				kind: "error",
+				message: `Unknown request kind: ${request.Kind}`,
+			};
+	}
 }
 
-rl.on('line', (input) => {
-    var request: SpellRequest = JSON.parse(atob(input)) 
-    console.log("input: " + input)
-})
+rl.on("line", async (input) => {
+	var request: SpellRequest = JSON.parse(atob(input));
+	var response = await processRequest(request);
+	console.log(JSON.stringify(response));
+});
 
 await new Promise(() => {});
