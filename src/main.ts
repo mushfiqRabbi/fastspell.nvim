315cb9c706d8b5f4254bb379c2e8ea3a4306d293
Luca Sartore <lucasar.com8@gmail.com>
2025-02-24 10:47:49 +0100

    Add input structure

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,49 @@
 import { spellCheckDocument } from 'cspell-lib';
+import type { ValidationIssue } from 'cspell-lib';
+import type {SpellRequest, PartialLintRequest} from './types/requests.ts';
+import type {SpellResponse, LintResponse} from './types/responses.ts';
+import { convertLintResult } from './util/lintResultConverter.ts';
+import readline from 'node:readline'
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
+async function processPartialLintRequest(request: PartialLintRequest ){
+  const result = await spellCheckDocument(
+    { uri: '', text: request.text, languageId: request.languageId, locale: 'en' },
+    { noConfigSearch: true },
+    { }
+  );
+  return convertLintResult(result.issues, request.startLine)
+}
 
-
-async function checkSpelling(phrase: string) {
+async function checkSpelling(phrase: string): Promise<Array<ValidationIssue>> {
   const result = await spellCheckDocument(
     { uri: '', text: phrase, languageId: 'plaintext', locale: 'en,it' },
-    {  noConfigSearch: true },
+    { noConfigSearch: true },
     { }
   );
   return result.issues;
 }
 
-export async function run() {
-  console.log(`Start: ${new Date().toISOString()}`);
-  const r = await checkSpelling('These are my coztom wordz. Processore');
-  console.log(`End: ${new Date().toISOString()}`);
-  //console.log('%o', r);
+
+async function processRequest(request: SpellRequest): Promise<SpellResponse>{
+    switch (request.Kind){
+        case "partial":
+            return await processPartialLintRequest(request);
+        default:
+            return {
+                kind: "error",
+                message: `Unknown request kind: ${request.Kind}`
+            }
+    }
 }
 
-await run();
-await run();
-await run();
+rl.on('line', (input) => {
+    var request: SpellRequest = JSON.parse(atob(input)) 
+    console.log("input: " + input)
+})
+
+await new Promise(() => {});
