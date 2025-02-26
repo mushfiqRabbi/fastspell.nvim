8b24d7342998111f10d70d78e16179cb5651c923
lucaSartore <lucasar.com8@gmail.com>
2025-02-26 19:14:34 +0100

    make requests queueable

--- lua/requests/spell_check_request.lua
+++ lua/requests/spell_check_request.lua
@@ -3,6 +3,7 @@ local M = {}
 ---@class Request
 ---@field is_in_execution boolean inclusive, 0 indexed
 ---@field in_queue boolean exclusive
+---@field next_request_args SpellCheckRequestArgs|nil
 
 ---@class SpellCheckRequestArgs
 ---@field line_start number
@@ -22,8 +23,8 @@ function M.setup(namespace, interface)
 end
 
 ---@return Request
-function M.getCurrentBufferRequests()
-	local current_buffer = vim.nvim_get_current_buf()
+function M.getCurrentBufferRequest()
+	local current_buffer = vim.api.nvim_get_current_buf()
 	if requests_by_buffer[current_buffer] == nil then
 		requests_by_buffer[current_buffer] = {
 			is_in_execution = false,
@@ -51,10 +52,29 @@ function M.processSpellCheckRequest(input)
 
 	vim.diagnostic.reset(M.namespace, vim.api.nvim_get_current_buf())
 	vim.diagnostic.set(M.namespace, vim.api.nvim_get_current_buf(), diagnostics)
+
+    local request = M.getCurrentBufferRequest()
+    request.is_in_execution = false
+
+    if request.in_queue then
+        request.in_queue = false
+        M.sendSpellCheckRequest(request.next_request_args)
+    else
+
+    end
 end
 
 ---@param args SpellCheckRequestArgs
 function M.sendSpellCheckRequest(args)
+
+    local request = M.getCurrentBufferRequest()
+    if request.is_in_execution then
+        request.next_request_args = args
+        request.in_queue = true
+        return
+    end
+
+    request.is_in_execution = true
 	local buffer = vim.api.nvim_get_current_buf()
 	local linesArray = vim.api.nvim_buf_get_lines(buffer, args.line_start, args.line_end, true)
 	local lines = table.concat(linesArray, "\n")
