60852fa2167beb6f2feddd4f769edc694dd1f8f1
lucaSartore <lucasar.com8@gmail.com>
2025-02-27 09:39:04 +0100

    use mroe generic plugin structure

--- config.lua
+++ config.lua
@@ -1,21 +1,12 @@
-local interface = require("lua.util.interface")
-local spell_check_request = require("lua.requests.spell_check_request")
+local fastspell = require("lua.fastspell")
 
-local namespace = vim.api.nvim_create_namespace("cspell")
-
-vim.diagnostic.config({
-    update_in_insert = true
-},namespace)
-
-
-spell_check_request.setup(namespace, interface)
-interface.setup(spell_check_request.processSpellCheckRequest)
+fastspell.setup()
 
 vim.api.nvim_create_autocmd({"TextChanged", "TextChangedI", "BufEnter", "WinScrolled"}, {
 	callback = function(_)
         local first_line = vim.fn.line('w0')
         local last_line = vim.fn.line('w$')
-        spell_check_request.sendSpellCheckRequest(first_line, last_line)
+        fastspell.sendSpellCheckRequest(first_line, last_line)
 
         -- local end_line = vim.api.nvim_buf_line_count(0)
         -- spell_check_request.sendSpellCheckRequest(0, end_line)
---
+++ lua/fastspell.lua
@@ -0,0 +1,29 @@
+local interface = require("lua.util.interface")
+local spell_check_request = require("lua.requests.spell_check_request")
+
+local M = {}
+
+---@class FastSpellSetup
+local default_settings = {
+    namespace = "fastspell"
+}
+
+function M.setup(user_settings)
+    ---@type FastSpellSetup
+    local settings = vim.tbl_deep_extend("force", default_settings, user_settings or {})
+
+    local namespace = vim.api.nvim_create_namespace(settings.namespace)
+
+    vim.diagnostic.config({
+        update_in_insert = true
+    },namespace)
+
+
+    spell_check_request.setup(namespace, interface)
+    interface.setup(spell_check_request.processSpellCheckRequest)
+
+    M.sendSpellCheckRequest = spell_check_request.sendSpellCheckRequest
+
+end
+
+return M
