06ad911e331d6a08854d6ff0fbd9a57af30be1a1
Luca Sartore <lucasar.com8@gmail.com>
2025-03-03 12:08:16 +0100

    fix 0-indexed lines issue

--- lua/config.lua
+++ lua/config.lua
@@ -4,7 +4,7 @@ fastspell.setup()
 
 vim.api.nvim_create_autocmd({"TextChanged", "TextChangedI", "BufEnter", "WinScrolled"}, {
 	callback = function(_)
-        local first_line = vim.fn.line('w0')
+        local first_line = vim.fn.line('w0')-1
         local last_line = vim.fn.line('w$')
         fastspell.sendSpellCheckRequest(first_line, last_line)
 	end
